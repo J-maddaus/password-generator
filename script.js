651d1ccf3324c5d20e7f7e41b5fead496c1db594
jacob maddaus <jakemaddaus@outlook.com>
2023-12-22 22:47:08 -0600

    updated the javascript to be better formated for readability

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-    function getPassword() {
-    
+function getPassword() {
+
     // Password variables for all available character options.
     var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
@@ -15,38 +15,38 @@ var generateBtn = document.querySelector("#generate");
     var keyLength = prompt(
         "Please choose a password length between 8 and 128 characters long."
     );
-    
+
     //prompt if the password length is below 8 characters or above 128 characters gives error message and ask user to try again
     if (keyLength < 8 || keyLength > 128 || isNaN(keyLength)) {
-        alert("Invaild choice password length must be between  8 and 128 characters. Please try again.");        
+        alert("Invaild choice password length must be between  8 and 128 characters. Please try again.");
         return;
 
-    // these variables prompt the user to select what characters the would like to include. by selecting ok the characters are included and by selecting cancel they are excluded.
-      } else {
-    var upperCase = confirm("Click OK if you would like uppercase letters to be included. Click cancel if not.");
-    var lowerCase = confirm("Click OK if you would like lowercase letters to be included. Click cancel if not.");
-    var numbers = confirm("Click OK if you would like numbers to be included. Click cancel if not.");
-    var symbolChar = confirm("Click OK if you would like symbols to be included. Click cancel if not.");
+        // these variables prompt the user to select what characters the would like to include. by selecting ok the characters are included and by selecting cancel they are excluded.
+    } else {
+        var upperCase = confirm("Click OK if you would like uppercase letters to be included. Click cancel if not.");
+        var lowerCase = confirm("Click OK if you would like lowercase letters to be included. Click cancel if not.");
+        var numbers = confirm("Click OK if you would like numbers to be included. Click cancel if not.");
+        var symbolChar = confirm("Click OK if you would like symbols to be included. Click cancel if not.");
     }
 
-    if (upperCase) {charactersSelected += upperCaseLetters;}
-    if (lowerCase) {charactersSelected += lowerCaseLetters;}
-    if (numbers) {charactersSelected += numberSymbols;}
-    if (symbolChar) {charactersSelected += symbols;}
+    if (upperCase) { charactersSelected += upperCaseLetters; }
+    if (lowerCase) { charactersSelected += lowerCaseLetters; }
+    if (numbers) { charactersSelected += numberSymbols; }
+    if (symbolChar) { charactersSelected += symbols; }
     if (upperCase === false && lowerCase === false && numbers === false && symbolChar === false) {
-      alert (" Atleast one character type needs to be selected. Please try again")
+        alert(" Atleast one character type needs to be selected. Please try again")
     }
 
     for (i = 0; i < keyLength; i++) {
 
         newPassword += charactersSelected[Math.floor(Math.random() * charactersSelected.length)]
     }
-    return(newPassword);
+    return (newPassword);
 }
 
 // Write password to the #password input.
-    var passwordText = document.querySelector("#password");
-    function writePassword(newPassword) {
+var passwordText = document.querySelector("#password");
+function writePassword(newPassword) {
     if (password.length === 0) {
         return;
     }
@@ -54,6 +54,5 @@ var generateBtn = document.querySelector("#generate");
 }
 
 // Add event listener to generate button.
-    generateBtn.addEventListener("click", function () { writePassword(getPassword()) });
+generateBtn.addEventListener("click", function () { writePassword(getPassword()) });
 
-    
