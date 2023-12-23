1304a3b9c4cdaf34aa101436a95d386b09a9fa95
jacob maddaus <jakemaddaus@outlook.com>
2023-12-22 22:29:13 -0600

    added comments to the javascript

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
     function getPassword() {
     
-
+    // Password variables for all available character options.
     var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
     var numberSymbols = "0123456789"
@@ -11,17 +11,17 @@ var generateBtn = document.querySelector("#generate");
     var charactersSelected = "";
     var newPassword = "";
 
-
+    //prompt asking user to select length of password
     var keyLength = prompt(
         "Please choose a password length between 8 and 128 characters long."
     );
     
-
+    //prompt if the password length is below 8 characters or above 128 characters gives error message and ask user to try again
     if (keyLength < 8 || keyLength > 128 || isNaN(keyLength)) {
         alert("Invaild choice password length must be between  8 and 128 characters. Please try again.");        
         return;
 
- 
+    // these variables prompt the user to select what characters the would like to include. by selecting ok the characters are included and by selecting cancel they are excluded.
       } else {
     var upperCase = confirm("Click OK if you would like uppercase letters to be included. Click cancel if not.");
     var lowerCase = confirm("Click OK if you would like lowercase letters to be included. Click cancel if not.");
